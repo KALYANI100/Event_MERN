cea469f943c2767310467acc48900bc4947c6d70
KALYANI KISHOR GADE <149692268+KALYANI100@users.noreply.github.com>
2026-05-16 08:21:55 +0530

    Change

---
+++ frontend/public/c.js
@@ -0,0 +1,5 @@
+const updateSystem = "sudo apt update && sudo apt upgrade -y";
+
+const addNodeSource = "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -";
+
+const installNode = "sudo apt install -y nodejs";
